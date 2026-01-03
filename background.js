e607a35d6731c892e3a5b831afc7a3d17abb0f05
Umut Yalçın <artabqos251@gmail.com>
2026-01-03 19:38:46 +0300

    Remove commented-out code for clarity

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-// Handle extension icon click
+
 chrome.action.onClicked.addListener((tab) => {
     if (tab.id) {
         chrome.tabs.sendMessage(tab.id, { action: "toggle_badge" });
     }
 });
+
